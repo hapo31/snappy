a4584ca7aa3415330eb2da97f31c4b8a01734fe2
happou31 <happo31@gmail.com>
2017-04-13 01:17:00 +0900

    イベントハンドラを定義

--- DnDScreenCapture/View/OAuthWindow.xaml.cs
+++ DnDScreenCapture/View/OAuthWindow.xaml.cs
@@ -25,8 +25,36 @@ namespace DnDScreenCapture.View
             InitializeComponent();
             var vm = new OAuthWindowViewModel();
             this.OAuthBrowser.Source = oauthUri;
+            OAuthBrowser.Navigating += (sender, obj) =>
+            {
+                var url = obj.Uri.ToString();
+                Console.WriteLine($"navigation:{url}");
+                if(url.IndexOf(Properties.Resources.CallbackScheme) >= 0)
+                {
+                    var token = url.Substring(url.IndexOf("?oauth_token=") + 13, 27);
+                    var verifier = url.Substring(url.IndexOf("&oauth_verifier=") + 16);
+                    Console.WriteLine($"token:{token} verifier:{verifier}");
+                    oauthCallbackHandler?.Invoke(new OAuthCallbackEventArgs(token, verifier));
+                }
+
+            };
+            //new System.Windows.Navigation.NavigatingCancelEventHandler()
             this.DataContext = vm;
             vm.Initialized(oauthUri);
+        }
+        
+        public event OAuthCallbackHandler oauthCallbackHandler;
+
+        public delegate void OAuthCallbackHandler(OAuthCallbackEventArgs ev);
+        public class OAuthCallbackEventArgs: EventArgs
+        {
+            public string Token { get; set; }
+            public string Verifier { get; set; }
+            public OAuthCallbackEventArgs(string token, string verifier)
+            {
+                Token = token;
+                Verifier = verifier;
+            }
         }
     }
 }
