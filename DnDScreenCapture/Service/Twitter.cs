07571b71e5e73c57ccdddba14b2662a13a46d133
happou31 <happo31@gmail.com>
2017-04-13 01:16:36 +0900

    色々追加 ・verifierを使ってAccessTokenを取得するメソッドを追加 ・コメントを追加

--- DnDScreenCapture/Service/Twitter.cs
+++ DnDScreenCapture/Service/Twitter.cs
@@ -3,6 +3,7 @@ using System.Xml.Serialization;
 
 using CoreTweet;
 using System;
+using System.Threading.Tasks;
 
 namespace DnDScreenCapture.Service
 {
@@ -15,6 +16,9 @@ namespace DnDScreenCapture.Service
         private string ConsumerSecret { get; set; }
         public Tokens Token { get; set; }
 
+        /// <summary>
+        /// OAuth認証が必要かどうかを返すプロパティ
+        /// </summary>
         public bool AuthorizationRequired
         {
             get
@@ -23,6 +27,11 @@ namespace DnDScreenCapture.Service
             }
         }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="consumer_key">アプリのコンシューマキー</param>
+        /// <param name="consumer_secret">アプリのコンシューマシークレットキー</param>
         public Twitter(string consumer_key, string consumer_secret)
         {
             ConsumerKey = consumer_key;
@@ -32,6 +41,11 @@ namespace DnDScreenCapture.Service
             Token.ConsumerSecret = consumer_secret;
         }
 
+        /// <summary>
+        /// PINコードによる認証を行います
+        /// </summary>
+        /// <param name="PINCode">ブラウザ上で取得したPINコード</param>
+        /// <returns></returns>
         public bool OAuth(string PINCode = "")
         {
             if (string.IsNullOrEmpty(PINCode))
@@ -40,6 +54,7 @@ namespace DnDScreenCapture.Service
                 try
                 {
                     session = CoreTweet.OAuth.Authorize(ConsumerKey, ConsumerSecret);
+
                     var url = session.AuthorizeUri;
                     System.Diagnostics.Process.Start(url.ToString());
                 }
@@ -59,13 +74,34 @@ namespace DnDScreenCapture.Service
             return true;
         }
 
-        public Uri GetOAuthUri()
+        /// <summary>
+        /// OAuth認証のためのURLを取得します
+        /// </summary>
+        /// <param name="callbackUrl">コールバックURL</param>
+        /// <returns></returns>
+        public Uri GetOAuthUri(string callbackUrl)
         {
-            session = CoreTweet.OAuth.Authorize(ConsumerKey, ConsumerSecret);
+            session = CoreTweet.OAuth.Authorize(ConsumerKey, ConsumerSecret, callbackUrl);
             return session.AuthorizeUri;
         }
 
-        public bool SaveToken(string filename)
+        /// <summary>
+        /// OAuthVerifierを用いてアクセストークンを取得します
+        /// </summary>
+        /// <param name="verifier">OAuth認証で得られたverifier</param>
+        /// <returns></returns>
+        public async Task<Tokens> GetTokensByVerifierAsync(string verifier)
+        {
+            return await CoreTweet.OAuth.GetTokensAsync(session, verifier);
+        }
+
+        /// <summary>
+        /// 取得したトークンを指定したファイル名で保存します
+        /// </summary>
+        /// <param name="filename">ファイル名</param>
+        /// <param name="token">保存するトークン なければ内部で保持しているトークンを保存</param>
+        /// <returns></returns>
+        public bool SaveToken(string filename, Tokens token = null)
         {
             try
             {
@@ -74,7 +110,8 @@ namespace DnDScreenCapture.Service
 
                     var sr = new XmlSerializer(typeof(TokenData));
                     var data = new TokenData();
-
+                    // トークンの取得もしくは何もしない
+                    Token = token ?? Token;
                     data.access_token = Token.AccessToken;
                     data.access_token_secret = Token.AccessTokenSecret;
                     sr.Serialize(sw, data);
@@ -88,6 +125,11 @@ namespace DnDScreenCapture.Service
             return true;
         }
 
+        /// <summary>
+        /// 指定した名前のファイルからトークンを読み込みます
+        /// </summary>
+        /// <param name="filename">ファイル名</param>
+        /// <returns></returns>
         public bool LoadToken(string filename)
         {
             try
